49e62b68e9384b662aa41e542284c96f4c0de690
Jiya609 <jjiya6764@gmail.com>
2026-09-07 00:05:24 +0530

    Implement power calculation in Assgn1_q9.java

---
+++ Assgn1_q9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int base = sc.nextInt();
+        int exponent = sc.nextInt();
+
+        long result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result = result * base;
+        }
+
+        System.out.println(result);
+    }
+}
